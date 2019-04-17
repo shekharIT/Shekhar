e2ffb821706dd97fe31949cc7466df5fd94f8703
Chandrashekhar Kumar <shekharkumar20kumar@gmail.com>
2019-04-17 10:05:15 +0530

    Unwanted files

--- LoginViaVolleyPhp/app/src/main/java/com/example/utils/URLs.java
+++
@@ -1,8 +0,0 @@
-package com.example.utils;
-
-public class URLs {
-
-    private static final String ROOT_URL = "http://192.168.43.104/Android/androidphpmysql/registrationapi.php?apicall=";
-    public static final String URL_REGISTER = ROOT_URL + "signup";
-    public static final String URL_LOGIN= ROOT_URL + "login";
-}
